fa4b8c2251f0b943c6d5f298c738c51f6afab2b4
Mateusz040 <6001634@mborijnland.nl>
2022-06-08 15:05:16 +0200

    Ala power

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Power energy">
+    <meta name="keywords" content="HTML, CSS, JavaScript, PHP, SQL">
+    <meta name="author" content="Mateusz, Mats, Michael, Esat">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">News</a></li>
+            <li><a href="contact.asp">Contact</a></li>
+            <li><a href="about.asp">About</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
