9df9fc8cae046ccd0e4abf389e84b194bdfbaf6d
Mateusz040 <6001634@mborijnland.nl>
2022-06-09 13:56:11 +0200

    nav

--- index.php
+++ index.php
@@ -13,19 +13,23 @@
     <title>Power Drink</title>
 </head>
 <body>
-    <nav>
+<article id="flex-container">
+    <nav class="navbar">
         <img src="images/logo.png" alt="logo">
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="producten.php">Producten</a></li>
             <li><a href="events.php">Events</a></li>
             <li><a href="aanbiedingen.php">Aanbiedingen</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="contact.php">Contact</a></li>  
         </ul>
     </nav>
     <div class="foto-drankjes">
         <img src="images/art.png" alt="art">
-        <img src="images/unleash.png" alt="">
     </div>
+    <div class="foto2">
+        <img src="images/unleash.png" alt="unleash">
+    </div>
+</article>
 </body>
 </html>
