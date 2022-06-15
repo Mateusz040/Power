36f38974b134e11bf20ace6fb19fee10b8cd3bb6
Mateusz040 <6001634@mborijnland.nl>
2022-06-15 16:22:33 +0200

    aanbiedinge

--- contact.php
+++ contact.php
@@ -19,7 +19,7 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="producten.php">Producten</a></li>
             <li><a href="events.php">Events</a></li>
-            <li><a href="aanbiedingen.php">Aanbidingen</a></li>
+            <li><a href="aanbiedingen.php">Aanbiedingen</a></li>
             <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
